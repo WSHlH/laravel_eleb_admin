0fd671fbb18a2d612cae0ad6b9c81a3854db9c02
WSHlH <1764674098@qq.com>
2018-04-19 21:48:05 +0800

    添加店铺分类,添加管理员

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::resource('user','UserController');
+Route::resource('category','BusinessCategoryController');
